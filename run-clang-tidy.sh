36560f71e739e98fdb397e85babf80dd8ce9beb1
Falk Werner <falk.werner@gmx.net>
2019-02-22 22:59:10 +0100

    use $\(\) instead of ``

--- run-clang-tidy.sh
+++ run-clang-tidy.sh
@@ -2,7 +2,7 @@
 #
 # Helper to run clang-tidy to perform some additional checks.
 
-FILES=`find lib -name '*.c'`
-INCLUDES="-Iinclude -Ilib `pkg-config fuse3 --cflags` `pkg-config libwebsockets --cflags`"
+FILES=$(find lib -name '*.c')
+INCLUDES="-Iinclude -Ilib $(pkg-config fuse3 --cflags) $(pkg-config libwebsockets --cflags)"
 
 clang-tidy -header-filter='.*' ${FILES} -- ${INCLUDES}
