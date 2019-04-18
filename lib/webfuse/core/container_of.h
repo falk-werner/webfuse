c14ad18ca87d34519799774a018f6d0da2df1fc2
Falk Werner <falk.werner@gmx.net>
2019-04-18 19:21:07 +0200

    makes WF_CONTAINER_OF use typeof, if available

--- lib/webfuse/core/container_of.h
+++ lib/webfuse/core/container_of.h
@@ -7,7 +7,15 @@
 #include <cstddef>
 #endif
 
+#ifdef __GNUC__
+#define WF_CONTAINER_OF(pointer, type, member) \
+    ({ \
+        const typeof( ((type *)0)->member ) * __member = (pointer); \
+        (type *)( (char *)__member - offsetof(type, member) ); \
+    })
+#else
 #define WF_CONTAINER_OF(pointer, type, member) \
     (type *)  (((char *) pointer) - offsetof(type, member))
+#endif
 
 #endif
