e2ff66b4c102ccb9541a55f513ba9b78c2b813a2
Falk Werner <falk.werner@gmx.net>
2019-04-21 20:36:46 +0200

    added matcher of readdir results

--- test/test_static_filesystem.cc
+++ test/test_static_filesystem.cc
@@ -9,21 +9,35 @@
 using webfuse_test::request_create;
 using webfuse_test::MockRequest;
 using webfuse_test::GetAttrMatcher;
+using webfuse_test::ReaddirMatcher;
 using testing::_;
 
-TEST(wfp_static_filesystem, init)
+TEST(wfp_static_filesystem, has_root_dir)
 {
     struct wfp_client_config * config = wfp_client_config_create();
     struct wfp_static_filesystem * filesystem = wfp_impl_static_filesystem_create(config);
 
     MockRequest mock;
     struct wfp_request * request = request_create(&mock, 42);
-
     EXPECT_CALL(mock, respond(GetAttrMatcher(1, 0555, "dir"), 42)).Times(1);
 
     config->provider.getattr(request, 1, config->user_data);
 
+    wfp_impl_static_filesystem_dispose(filesystem);
+    wfp_client_config_dispose(config);
+}
 
+TEST(wfp_static_filesystem, contains_default_dirs)
+{
+    struct wfp_client_config * config = wfp_client_config_create();
+    struct wfp_static_filesystem * filesystem = wfp_impl_static_filesystem_create(config);
+
+    MockRequest mock;
+    struct wfp_request * request = request_create(&mock, 23);
+    char const * default_dirs[] = {".", "..a", nullptr};
+    EXPECT_CALL(mock, respond(ReaddirMatcher(default_dirs), 23)).Times(1);
+
+    config->provider.readdir(request, 1, config->user_data);
 
     wfp_impl_static_filesystem_dispose(filesystem);
     wfp_client_config_dispose(config);
