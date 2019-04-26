029b77eed6bcb1a78052e8a162796a905add5c92
Falk Werner <falk.werner@gmx.net>
2019-04-26 22:39:39 +0200

    uses cmake to generate coverage

--- create_coverage.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-#
-# Note: lcov is needed to generate coverage report
-#
-
-mkdir -p .build/coverage
-lcov --capture --directory . --output-file .build/coverage/lcov.info
-genhtml .build/coverage/lcov.info --output-directory .build/coverage/report
