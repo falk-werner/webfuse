fae1cd6d5441df9de03b7146a1b4035409ccbb3a
Falk Werner <falk.werner@gmx.net>
2019-05-25 02:47:10 +0200

    removes unused scripts

--- run-clang-tidy.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-#
-# Helper to run clang-tidy to perform some additional checks.
-
-FILES=$(find lib -name '*.c')
-INCLUDES="-Iinclude -Ilib $(pkg-config fuse3 --cflags) $(pkg-config libwebsockets --cflags)"
-
-clang-tidy -header-filter='.*' ${FILES} -- ${INCLUDES}
