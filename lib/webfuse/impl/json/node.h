6c5ff67720106e3f8835f980ea87fa477980aee0
Falk Werner <falk.werner@gmx.net>
2020-07-14 21:46:01 +0200

    added implementation of json parser

---
+++ lib/webfuse/impl/json/node.h
@@ -0,0 +1,77 @@
+#ifndef WF_IMPL_JSON_NODE_H
+#define WF_IMPL_JSON_NODE_H
+
+#ifndef __cplusplus
+#include <stdbool.h>
+#include <stddef.h>
+#else
+#include <cstddef>
+#endif
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+enum wf_json_type
+{
+    WF_JSON_TYPE_UNDEFINED,
+    WF_JSON_TYPE_NULL,
+    WF_JSON_TYPE_BOOL,
+    WF_JSON_TYPE_INT,
+    WF_JSON_TYPE_STRING,
+    WF_JSON_TYPE_ARRAY,
+    WF_JSON_TYPE_OBJECT
+};
+
+struct wf_json;
+
+extern enum wf_json_type
+wf_impl_json_type(
+    struct wf_json const * json);
+
+extern bool
+wf_impl_json_bool_get(
+    struct wf_json const * json);
+
+extern int
+wf_impl_json_int_get(
+    struct wf_json const * json);
+
+extern char const *
+wf_impl_json_string_get(
+    struct wf_json const * json);
+
+extern struct wf_json const *
+wf_impl_json_array_get(
+    struct wf_json const * json,
+    size_t pos);
+
+extern size_t
+wf_impl_json_array_size(
+    struct wf_json const * json);
+
+extern struct wf_json const *
+wf_impl_json_object_get(
+    struct wf_json const * json,
+    char const * key);
+
+extern size_t
+wf_impl_json_object_size(
+    struct wf_json const * json);
+
+extern char const *
+wf_impl_json_object_key(
+    struct wf_json const * json,
+    size_t pos);
+
+extern struct wf_json const *
+wf_impl_json_object_value(
+    struct wf_json const * json,
+    size_t pos);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
---
+++ lib/webfuse/impl/json/reader.c
@@ -0,0 +1,179 @@
+#include "webfuse/impl/json/reader.h"
+
+#include <string.h>
+
+static char
+wf_impl_json_unescape(
+    char c);
+
+void
+wf_impl_json_reader_init(
+    struct wf_json_reader * reader,
+    char * contents,
+    size_t length)
+{
+    reader->contents = contents;
+    reader->length = length;
+    reader->pos = 0;
+}
+
+char
+wf_impl_json_reader_skip_whitespace(
+    struct wf_json_reader * reader)
+{
+    char c = wf_impl_json_reader_peek(reader);
+    if ((' ' == c) || ('\n' == c) || ('\t' == c) || ('\r' == c))
+    {
+        reader->pos++;
+        c = wf_impl_json_reader_peek(reader);
+    }
+
+    return c;
+}
+
+char
+wf_impl_json_reader_peek(
+    struct wf_json_reader * reader)
+{
+    char result = '\0';
+    if (reader->pos < reader->length)
+    {
+        result = reader->contents[reader->pos];
+    }
+
+    return result;
+}
+
+char
+wf_impl_json_reader_get_char(
+    struct wf_json_reader * reader)
+{
+    char result = '\0';
+    if (reader->pos < reader->length)
+    {
+        result = reader->contents[reader->pos++];
+    }
+
+    return result;
+}
+
+void
+wf_impl_json_reader_unget_char(
+    struct wf_json_reader * reader)
+{
+    if (0 < reader->pos)
+    {
+        reader->pos--;
+    }
+}
+
+bool
+wf_impl_json_reader_read_const(
+    struct wf_json_reader * reader,
+    char const * value,
+    size_t length)
+{
+    size_t remaining = reader->length - reader->pos;
+    bool const result = ((remaining >= length) && (0 == strncmp((&reader->contents[reader->pos]), value, length)));
+    if (result)
+    {
+        reader->pos += length;
+    }
+
+    return result;
+}
+
+bool
+wf_impl_json_reader_read_int(
+    struct wf_json_reader * reader,
+    int * value)
+{
+    char c = wf_impl_json_reader_get_char(reader);
+    bool const is_signed = ('-' == c);
+    if (is_signed)
+    {
+        c = wf_impl_json_reader_get_char(reader);
+    }
+
+    bool const result = (('0' <= c) && (c <= '9'));
+    if (result)
+    {
+        int v = c - '0';
+        c = wf_impl_json_reader_peek(reader);
+        while (('0' <= c) && (c <= '9'))
+        {
+            v *= 10;
+            v += c - '0';
+            reader->pos++;
+            c = wf_impl_json_reader_peek(reader);
+        }
+
+        *value = (is_signed) ? -v : v;
+    }
+
+    return result;
+}
+
+bool
+wf_impl_json_reader_read_string(
+    struct wf_json_reader * reader,
+    char * * value)
+{
+    wf_impl_json_reader_skip_whitespace(reader);
+    char c = wf_impl_json_reader_get_char(reader);
+    if ('\"' == c) { return value; }
+
+    size_t p = reader->pos;
+    *value = &(reader->contents[p]);
+    c = wf_impl_json_reader_get_char(reader);
+    while (('\"' != c) && ('\"' != c))
+    {
+        if ('\\' != c)
+        {
+            reader->contents[p++] = c;
+        }
+        else
+        {
+            char unescaped = wf_impl_json_unescape(wf_impl_json_reader_get_char(reader));
+            if ('\0' != c)
+            {
+                reader->contents[p++] = unescaped;
+
+            }
+            else
+            {
+                *value = NULL;
+                return false;
+            }
+        }
+
+        c = wf_impl_json_reader_get_char(reader);
+    }
+
+    bool const result = ('\"' == c);
+    if (result)
+    {
+        reader->contents[p] = '\0';
+    }
+
+    return result;
+}
+
+static char
+wf_impl_json_unescape(
+    char c)
+{
+    switch (c)
+    {
+        case '\"': return '\"';
+        case '\\': return '\\';
+        case '/': return '/';
+        case 'b': return '\b';
+        case 'f': return '\f';
+        case 'n': return '\n';
+        case 'r': return '\r';
+        case 't': return '\t';
+        default:
+            return '\0';
+    }
+}
