d02baad53789ed835fda5540719877b6bbd2af5c
Falk Werner <falk.werner@gmx.net>
2025-02-21 20:52:28 +0100

    enable development using nix
    Signed-off-by: Falk Werner <falk.werner@gmx.net>

---
+++ flake.nix
@@ -0,0 +1,35 @@
+{
+  description = "A Nix-flake-based C/C++ development environment";
+
+  inputs.nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0.1.*.tar.gz";
+
+  outputs = { self, nixpkgs }:
+    let
+      supportedSystems = [ "x86_64-linux" ];
+      forEachSupportedSystem = f: nixpkgs.lib.genAttrs supportedSystems (system: f {
+        pkgs = import nixpkgs { inherit system; };
+      });
+    in
+    {
+      devShells = forEachSupportedSystem ({ pkgs }: {
+        default = pkgs.mkShell.override
+          {
+            # Override stdenv in order to change compiler:
+            # stdenv = pkgs.clangStdenv;
+          }
+          {
+            packages = with pkgs; [
+              pkg-config
+              cmake              
+              gtest
+              valgrind
+              gdb
+              clang-tools
+              openssl
+              fuse3
+              libwebsockets
+            ];
+          };
+      });
+    };
+}
